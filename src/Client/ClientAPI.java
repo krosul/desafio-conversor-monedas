4dc134cf6d4c3e9b15a82857144f8b63215db1a7
Juan Pablo Rodriguez <96890522+krosul@users.noreply.github.com>
2024-06-03 11:29:27 -0500

    create class for fecth api

---
+++ src/Client/ClientAPI.java
@@ -0,0 +1,60 @@
+package Client;
+
+import Models.CurrencyModel;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Currency;
+
+public class ClientAPI {
+    private static String key = "4be3e6c274eb7d3e89b3ea26";
+
+    private static String urlApi = "https://v6.exchangerate-api.com/v6/" + key + "/latest/USD";
+    public static Gson gson = new Gson();
+
+
+    private CurrencyModel makeRequest(String currency) {
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+//                .proxy(ProxySelector.of(new InetSocketAddress("proxy.example.com", 80)))
+//                .authenticator(Authenticator.getDefault())
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlApi))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body);
+//                .thenAccept(System.out::println);
+//        System.out.println(request);
+
+        try {
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            CurrencyModel resCurrency = gson.fromJson(response.body(), CurrencyModel.class);
+            System.out.println(resCurrency.conversion_rates.get("USD"));
+            return resCurrency;
+        } catch (Exception err) {
+            System.out.println(err);
+        }
+
+
+        return null;
+    }
+
+    public CurrencyModel getCurrency(String currency) {
+        CurrencyModel res = makeRequest(currency);
+//        System.out.println(res);
+        return res;
+    }
+}
---
+++ src/Models/CurrencyModel.java
@@ -0,0 +1,8 @@
+package Models;
+
+import java.util.HashMap;
+
+public class CurrencyModel {
+    public String base_code;
+    public HashMap<String, Double> conversion_rates;
+}
