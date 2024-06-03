b9a49f1bf6189686f8f900d6b86c7be015600a51
Juan Pablo Rodriguez <96890522+krosul@users.noreply.github.com>
2024-06-03 11:31:48 -0500

    add clases

--- src/Models/CurrencyAvialables.java
+++ src/Models/CurrencyAvialables.java
@@ -1,2 +1,15 @@
-package Models;public class CurrencyAvialables {
+package Models;
+
+import java.util.ArrayList;
+
+public class CurrencyAvialables {
+    public ArrayList<String> available = new ArrayList<>();
+
+
+    public CurrencyAvialables() {
+        available.add("USD");
+        available.add("COP");
+        available.add("ARS");
+        available.add("BRL");
+    }
 }
